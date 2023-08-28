11e2ce671cc70cd1ad7e42e5f16ed343ad722f2e
Sammy <sammy@tlg.com>
2023-08-28 17:22:25 +0000

    utils.py clean up

--- app/utils.py
+++ app/utils.py
@@ -8,20 +8,17 @@ APP_ID = "32aabb7c"
 APP_KEY = "2d280e0b89edb69ca1ca6fdf9f0d145a"
 
 def get_matching_recipes(ingredients):
-    #1st option formats ingredients as (x,y,z) vs 2nd (x, y, z) -- API changing it up
-    url = f"{API_URL}&q={'%2C'.join(ingredients)}&app_id={APP_ID}&app_key={APP_KEY}"
-    print(url)
-    response = requests.get(url)
+    #if API wants a space
+    #url = f"{API_URL}&q={'%2C%20'.join(ingredients)}&app_id={APP_ID}&app_key={APP_KEY}"
 
-    #response = requests.get(f"{API_URL}&q={'%2C%20'.join(ingredients)}&app_id={APP_ID}&app_key={APP_KEY}")
+    url = f"{API_URL}&q={'%2C'.join(ingredients)}&app_id={APP_ID}&app_key={APP_KEY}"
+    #print(url)
+    response = requests.get(url)
 
     if response.status_code == 200: #Ensuring API call was successful
         recipe_list = response.json()
-        ## BELOW LINE PERHAPS TROUBLE? ##
-        ## List: hits, dictionary: recipe, dictionary: label / source / url ##
         matching_recipes = recipe_list.get("hits", []) #Extracts list of recipes
         return matching_recipes
     else:
         error_message = "API call failed. Please try again later."
         return error_message
-
