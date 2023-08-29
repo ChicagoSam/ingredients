0e8ee91fb6e1485bf9bfc76ad2d9e896fc335aa5
Osbjorn6352 <osbjorn6352@gmail.com>
2023-08-29 14:29:15 +0000

    Added to testing file

--- app/test_app.py
+++ app/test_app.py
@@ -1,1 +1,8 @@
-#PLACEHOLDER FILE
+import utils
+import pytest
+
+lsw_url = ""
+
+def test_get_matching_recipes():
+    assert get_matching_recipes("lemon,sugar,water") == requests.get(lsw_url).json().get("hits")
+    assert 
