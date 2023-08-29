0be7fdd35a5b50fabeda765cdf6e61a60900dfe7
Sammy <sammy@tlg.com>
2023-08-29 22:09:54 +0000

    K8s manifest + commenting code

--- app/utils.py
+++ app/utils.py
@@ -8,13 +8,22 @@ APP_ID = "32aabb7c"
 APP_KEY = "2d280e0b89edb69ca1ca6fdf9f0d145a"
 
 def get_matching_recipes(ingredients):
-    #if API wants a space
-    #url = f"{API_URL}&q={'%2C%20'.join(ingredients)}&app_id={APP_ID}&app_key={APP_KEY}"
+    """
+    Get recipes that match the provided ingredients.
 
+    Args:
+        ingredients (list): List of ingredients for recipe search.
+
+    Returns:
+        list: List of dictionaries, each dictionary representing a different recipe.
+    """
+
+    ## Build the API URL using the provided ingredients, APP ID, and APP KEY
     url = f"{API_URL}&q={'%2C'.join(ingredients)}&app_id={APP_ID}&app_key={APP_KEY}"
+    # Make a GET request to the API
     response = requests.get(url)
 
-    if response.status_code == 200: #Ensuring API call was successful
+    if response.status_code == 200: #Check if API call was successful
         recipe_list = response.json()
         matching_recipes = recipe_list.get("hits", []) #Extracts list of recipes
         return matching_recipes
