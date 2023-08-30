5f4e96ba03fb51070fb8d88dff4685508431bf7c
Osbjorn6352 <osbjorn6352@gmail.com>
2023-08-30 14:42:44 +0000

    Reinserted variable into test_app.py

--- app/test_app.py
+++ app/test_app.py
@@ -3,6 +3,8 @@ import pytest
 from utils import get_matching_recipes
 import routes
 
+lsw_url = "https://api.edamam.com/api/recipes/v2?type=public&q=lemon%2Csugar%2Cwater&app_id=32aabb7c&app_key=2d280e0b89edb69ca1ca6fdf9f0d145a"
+
 #ensure utils returns the correct list
 def test_get_matching_recipes():
     function_list = get_matching_recipes(['lemon', 'sugar', 'water'])
